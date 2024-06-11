29d6aaff4601fe0a7bbfe6d1b7cb06f12cdf33eb
Francisco.V98 <frncsico@gmail.com>
2024-06-11 12:34:57 -0400

    add theme color

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFDC220);
+  static const Color secondary = Color(0xFF2B3E52);
+  static const Color infoLight = Color(0xFF279BD6);
+  static const Color bgTopLight = Color(0xFFFFFFFF);
+  static const Color bgBotLight = Color(0xFFEEEEEE);
+  static const Color black = Color(0xFF0E2133);
+  static const Color greyLight = Color(0xFFBFC3C6);
+}
+
