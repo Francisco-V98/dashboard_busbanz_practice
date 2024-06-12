585018488086c3c8b63a37dab5a766badd48f3a0
Francisco Colmenarez <156324130+Francisco-V98@users.noreply.github.com>
2024-06-12 13:51:59 -0400

    create appBar (#4)

--- lib/screens/Calendar/calendar_screen.dart
+++ lib/screens/Calendar/calendar_screen.dart
@@ -9,27 +9,58 @@ class CalendarScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.yellow,
       appBar: AppBar(title: const Text('Calendar'), centerTitle: true),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 40),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CalendarContainer(month: 'Ago', date: '05', day: 'Lun'),
-                  CalendarContainer(isActive: true, month: 'Ago', date: '05', day: 'Lun'),
-                  CalendarContainer(isToday: true, month: 'Ago', date: '05', day: 'Lun'),
-                  CalendarContainer(isToday: true, isActive: true, month: 'Ago', date: '05', day: 'Lun'),
-                ],
-              ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(vertical: 40),
+      child: Column(
+        children: [
+          AppBarBusbanz(
+            withBackground: false,
+            title: 'Hola, Freddy',
+            image: 'assets/images/imagen_de_conductor.png',
+          ),
+          SizedBox(height: 32),
+          AppBarBusbanz(
+            title: 'Hola, Freddy',
+            image: 'assets/images/imagen_de_conductor.png',
+          ),
+          SizedBox(height: 32),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CalendarContainer(month: 'Ago', date: '05', day: 'Lun'),
+                CalendarContainer(
+                    isActive: true, month: 'Ago', date: '05', day: 'Lun'),
+                CalendarContainer(
+                    isToday: true, month: 'Ago', date: '05', day: 'Lun'),
+                CalendarContainer(
+                    isToday: true,
+                    isActive: true,
+                    month: 'Ago',
+                    date: '05',
+                    day: 'Lun'),
+              ],
             ),
-            SizedBox(height: 16),
-            CalendarContainerGroup(),
-          ],
-        ),
+          ),
+          SizedBox(height: 16),
+          CalendarContainerGroup(),
+        ],
       ),
     );
   }
 }
+
+
+
+
