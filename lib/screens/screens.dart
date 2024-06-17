aee1a4097732b823b9dc1e46036d53e182155328
Francisco.V98 <frncsico@gmail.com>
2024-06-17 09:47:49 -0400

    ajust folders to fran

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,8 +1,8 @@
 
-export 'package:dashboard_busbanz_practice/screens/card/card_active_route.dart';
+export 'package:dashboard_busbanz_practice/screens/card/card_active_route_screen.dart';
 
-export 'package:dashboard_busbanz_practice/screens/card/card_default.dart';
+export 'package:dashboard_busbanz_practice/screens/card/card_default_screen.dart';
 
 export 'package:dashboard_busbanz_practice/screens/Calendar/calendar_screen.dart';
 
-export 'package:dashboard_busbanz_practice/screens/list_components.dart';
+export 'package:dashboard_busbanz_practice/screens/list_components_screen.dart';
